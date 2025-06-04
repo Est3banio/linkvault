00f1d2730af4eeea34e23c5c2a7434564c64d41e
Est3banio <est3banio.code@baniobits.dev>
2025-06-04 11:44:45 +0200

    UsersController class is properly defined. The Rails server should start without the Zeitwerk error.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
-# This controller is no longer needed as Devise handles user registration
-# Keeping the file as a reference in case we need to customize Devise controllers later
+class UsersController < ApplicationController
+  # This controller is no longer needed as Devise handles user registration
+  # Keeping the file as a reference in case we need to customize Devise controllers later
+end
