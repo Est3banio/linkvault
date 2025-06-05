3e390a242243782395e60fae22c161e0a9f1cab1
Est3banio <est3banio.code@baniobits.dev>
2025-06-05 06:50:03 +0200

    issue is now fixed. The Link model will properly handle the comma-separated tags string from the form by:
    1. Splitting the string by commas
      2. Trimming whitespace from each tag name
      3. Removing blank entries
      4. Finding or creating Tag records for each name
      5. Associating them with the link

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,7 +9,20 @@ class Link < ApplicationRecord
   has_many :tags, through: :link_tags
   before_save :fetch_meta_data, if: -> { url_changed? }
 
+  def tags=(value)
+    return unless value.is_a?(String)
+    
+    tag_names = value.split(',').map(&:strip).reject(&:blank?)
+    self.tag_objects = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+  end
+
   private
+
+  attr_writer :tag_objects
+
+  def tag_objects=(tag_array)
+    self.tags = tag_array
+  end
 
   def fetch_meta_data
     page = MetaInspector.new(url)
